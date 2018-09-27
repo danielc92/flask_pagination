7f84e81710094e87f427cb9e90ad845345b39e1b
daniel corcoran <danielc.92g@gmail.com>
2018-09-27 17:16:04 +1000

    Added working or_ and_ to ilike search

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #imports
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_, and_
 
 # app settings
 app = Flask(__name__)
@@ -47,16 +48,17 @@ def home(page_num):
 
     # IF SEARCH BUTTON IS PRESSED STORE SEARCH TERM IN SESSION AND REDIRECT
     if request.method == 'POST' and len(request.form['search']) > 0:
-
-        session['search_term'] = request.form['search']  
+        session['search_term'] = request.form['search'].split(' ') 
         return redirect(url_for('home', page_num = 1))
 
     # IF SEARCH BUTTON IS NOT PRESS, RETAIN SEARCH
     elif request.method == 'GET' and len(session['search_term']) > 0:
 
-        orders = Orders.query\
-        .filter(Orders.region.ilike('%{}%'.format(session['search_term'])))\
-        .order_by(Orders.order_id)\
+        conditions = []
+        for name in session['search_term']:
+            conditions.append(Orders.customer_name.ilike('%{}%'.format(name)))
+
+        orders = Orders.query.filter(or_(*conditions))\
         .paginate(page = page_num, per_page = config_pp, error_out = True)
         print(orders.total)
         return render_template('home.html', orders = orders, session = session)
@@ -83,4 +85,18 @@ def home(page_num):
 QUERYING EXAMPLES
 - Model.query.filter(Model.columnName.contains('sub_string'))
 - orders = Orders.query.filter(Orders.customer_name.ilike('H%%')).paginate(1,8,False).items
+
+
+from sqlalchemy import or_
+
+conditions = []
+for name in names:
+    conditions.append(Detail.sellers.ilike('%{}%'.format(name)))
+
+q = session.query(Detail).filter(
+    or_(*conditions)
+)
+
+
+
 '''
